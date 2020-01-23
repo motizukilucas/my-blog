629c9ba5b5c18257b10915d6fb310a1f9f734d93
Lucas Motizuki <motizuki@protonmail.com>
2020-01-23 17:42:39 -0300

    Adding wp-content folder

---
+++ wp-content/themes/motizukiblog/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Blog Template for Bootstrap</title>
+  </head>
+  <body>
+    work in progress
+  </body>
+</html>
