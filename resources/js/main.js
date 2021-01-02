aee8f3dd3d10ddd8903fe6ebfe119f371dd7d0ad
Lucas Motizuki <motizuki@protonmail.com>
2021-01-02 19:18:39 -0300

    update

---
+++ resources/js/main.js
@@ -0,0 +1,48 @@
+// @TODO welcome animation
+window.addEventListener('load', function () {
+    $(".page-wrapper").css("opacity", "1");
+//     $(".welcome").css("display", "flex");
+});
+
+// Hamburger
+var hamburger = $(".hamburger");
+
+hamburger.click(function() {
+    hamburger.toggleClass("is-active");
+});
+
+// ChartJS
+var config = {
+    type: 'pie',
+    data: {
+        datasets: [{
+            data: [
+                4,
+                5,
+                1,
+                1,
+            ],
+            backgroundColor: [
+            'rgb(255, 99, 132)', // red
+            'rgb(54, 162, 235)', // blue
+            'rgb(255, 205, 86)', //yellow
+            'rgb(75, 192, 192)', // green
+            ],
+            label: 'Dataset 1'
+        }],
+        labels: [
+            'Front-end',
+            'Back-end',
+            'Infrastructure',
+            'Others',
+        ]
+    },
+    options: {
+        responsive: true
+    }
+};
+
+window.onload = function() {
+    var ctx = document.getElementById('chart-area').getContext('2d');
+    window.myPie = new Chart(ctx, config);
+};
